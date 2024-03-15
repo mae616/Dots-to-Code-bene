5f91a1268ead5a1ae8925804d10d2a38d266cdd2
mae616 <oya2.mi@hotmail.co.jp>
2024-03-15 16:58:53 +0900

    fix width

--- src/app/_components/MessageCard.jsx
+++ src/app/_components/MessageCard.jsx
@@ -3,8 +3,8 @@ export default function MessageCard({}) {
     return (
         <div className="relative">
             <img src="https://placehold.jp/960x660.png" />
-            <div className="absolute top-0 left-0 bg-transparent w-full h-full">
-                <div className="mt-3 translate-x-[85%]">
+            <div className="absolute top-0 left-0 bg-transparent w-full h-full z-10">
+                <div className="absolute top-3 left-[85%] z-20 w-max">
                     <BsFillCloudDownloadFill className="text-3xl text-sky-600 text-opacity-60" />
                 </div>
             </div>
