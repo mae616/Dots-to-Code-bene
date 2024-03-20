03c0c1d90440883936c5ab383ce0b4bd7e7b2741
mae616 <oya2.mi@hotmail.co.jp>
2024-03-20 21:18:30 +0900

    fix

--- src/app/_components/LikeButton.jsx
+++ src/app/_components/LikeButton.jsx
@@ -1,45 +1,45 @@
 "use client";
-import { useState } from "react";
 import { useLike } from "@/app/_hook/useLike";
+import LoadingAnimation from "./LoadingAnimation";
 
 export default function LikeButton({
   isLiked = false,
   countOfLikes = 0,
   complimentId,
 }) {
-  const [isLikedState, setIsLikedState] = useState(isLiked);
-  const [countOfLikesState, setCountOfLikesState] = useState(countOfLikes);
-  const { addLike, removeLike } = useLike();
+  const { addLike, removeLike, isLoading } = useLike();
 
-  if (isLikedState) {
+  if (isLiked) {
     return (
-      <div
-        className="flex items-center hover:cursor-pointer px-2 z-10"
-        onClick={(e) => {
-          e.preventDefault();
-          setIsLikedState((like) => !like);
-          setCountOfLikesState((count) => count - 1);
-          removeLike(complimentId);
-        }}
-      >
-        <i className="pi pi-heart-fill h-[10px] text-pink-400 mr-1" />
-        <div className="mt-1.5">{countOfLikesState}</div>
-      </div>
+      <>
+        <div
+          className="flex items-center hover:cursor-pointer px-2 z-10"
+          onClick={(e) => {
+            e.preventDefault();
+            removeLike(complimentId);
+          }}
+        >
+          <i className="pi pi-heart-fill h-[10px] text-pink-400 mr-1" />
+          <div className="mt-1.5">{countOfLikes}</div>
+        </div>
+        <LoadingAnimation loading={isLoading} />
+      </>
     );
   } else {
     return (
-      <div
-        className="flex items-center hover:cursor-pointer px-2 z-10"
-        onClick={(e) => {
-          e.preventDefault();
-          setIsLikedState((like) => !like);
-          setCountOfLikesState((count) => count + 1);
-          addLike(complimentId);
-        }}
-      >
-        <i className="pi pi-heart h-[10px]  text-pink-400 mr-1" />
-        <div className="mt-1.5">{countOfLikesState}</div>
-      </div>
+      <>
+        <div
+          className="flex items-center hover:cursor-pointer px-2 z-10"
+          onClick={(e) => {
+            e.preventDefault();
+            addLike(complimentId);
+          }}
+        >
+          <i className="pi pi-heart h-[10px]  text-pink-400 mr-1" />
+          <div className="mt-1.5">{countOfLikes}</div>
+        </div>
+        <LoadingAnimation loading={isLoading} />
+      </>
     );
   }
 }
